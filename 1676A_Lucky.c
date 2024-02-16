f437011e775c845f3beece106a4f10fff3323363
DivyaMusahib <divyamusahib@gmail.com>
2024-02-16 20:09:35 +0530

    16 Feb 2024

---
+++ 1676A_Lucky.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int t,x;
+    scanf("%d",&t);
+
+    for(int i=0; i<t; i++){
+        scanf("%d",&x);
+        int firstthree =  ((x/1000)%10) + ((x/10000)%10) + ((x/100000)%10);
+        int lastthree = (x%10) + ((x/10)%10) + ((x/100)%10);
+        if(firstthree == lastthree) printf("YES\n");
+        else printf("NO\n");
+    }
+
+    return 0;
+}
