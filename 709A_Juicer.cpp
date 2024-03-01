4070a9fb234003a09948fa9b149d424e56449624
DivyaMusahib <divyamusahib@gmail.com>
2024-03-01 10:08:10 +0530

    1 March 2024 commit

--- 709A_Juicer.cpp
+++ 709A_Juicer.cpp
@@ -6,15 +6,18 @@ int main(){
     cin >> n >> b >> d;
     vector<int>arr(n);
     int count =0;
+    int throwcount =0;
 
     for(int i=0; i<n; i++){
         cin >> arr[i];
         if (arr[i]<=b) count= count + arr[i];
+        if(count > d){
+            throwcount++;
+            count = 0;
+        }
     }
 
-    if(count % d ==0 && count/d >= 1)  cout << count/d - 1;
-    else cout << count/d;
-
-
+    cout << throwcount;
+    
     return 0;
 }
