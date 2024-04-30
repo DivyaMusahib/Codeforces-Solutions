61a7a7ad09ce2e8270f29b25f431ed13cf2cd6f6
DivyaMusahib <divyamusahib@gmail.com>
2024-04-30 23:35:04 +0530

    30 April 2024 commit

---
+++ 1537B_Bad_Boy.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int length, breadth, i, j;
+        cin >> length >> breadth >> i >> j;
+
+        cout << 1 << " " << 1 << " "; // put ball in top right
+        cout << length << " " << breadth << "\n"; // put ball in bottom left
+    }
+}
---
+++ Array_Cloning_Technique.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int>arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+
+        sort(arr.begin(),arr.end());
+
+        int count =0;
+        int maxFreq =0;
+        for(int i=0; i<n-1; i++){
+            if(arr[i]==arr[i+1]) count++;
+            else{
+                maxFreq = max(count,maxFreq);
+                count=0;
+            }
+        }
+        maxFreq = max(count,maxFreq);
+
+        
+    }
+}
