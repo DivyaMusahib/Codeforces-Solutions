34d0d51765657ab5b1c21bd024ccc932984b6915
DivyaMusahib <divyamusahib@gmail.com>
2024-05-15 23:36:56 +0530

    15 May 2024 commit

---
+++ 1337B_Kana_And_Dragon_Quest_Game.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	int t;
+    cin >> t;
+    while(t--){
+        int x,n,m;
+        cin >> x >> n >> m;
+
+	    while (x>0 && n && (x/2+10)<x){
+            n--;
+            x= x/2 +10;
+        }
+
+	    if (x<=m*10) cout << "YES\n";
+        else cout << "NO\n";
+    }
+	return 0;
+}
