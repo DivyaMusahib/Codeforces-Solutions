22fc35994e313df058745955260f650294c528a1
DivyaMusahib <divyamusahib@gmail.com>
2024-06-13 22:34:16 +0530

    13 June 2024 commit

--- 579A_Raising_Bacteria.cpp
+++ 579A_Raising_Bacteria.cpp
@@ -1,3 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long int n,ans;
+    while (cin>>n)
+    {
+        ans=0;
+        while(n>1){
+            if(n%2==0)
+                n=n/2;
+            else
+            {
+                n=n-1;ans++;
+                }
+        }
+        cout<<ans+1<<endl;
+    }
+    return 0;
+}
+
 // #include<bits/stdc++.h>
 // using namespace std;
 
@@ -53,23 +74,23 @@
 
 
 
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int main(){
-    double n;
-    long long ans=1;
-    cin >> n;
-    long long temp = n;
+// int main(){
+//     double n;
+//     long long ans=1;
+//     cin >> n;
+//     long long temp = n;
 
-    // long long count = two_times(n,ans);
+//     // long long count = two_times(n,ans);
 
-    for(int i=0; (n/2 >= 1); i++){
-        n = n/2.0;
-        ans = ans * 2;
-        cout << n << " " << ans  << "\n";
-    }
+//     for(int i=0; (n/2 >= 1); i++){
+//         n = n/2.0;
+//         ans = ans * 2;
+//         cout << n << " " << ans  << "\n";
+//     }
 
-    cout << temp - ans + 1;
-    return 0;
-}
+//     cout << temp - ans + 1;
+//     return 0;
+// }
